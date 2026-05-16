080133fdcd576cb1999b76b27a96bd04bc097574
zooth98 <alejozambrano020@gmail.com>
2026-05-16 11:52:52 -0500

    Finalizacion del punto 1

--- solution_minesweeper.py
+++ solution_minesweeper.py
@@ -17,10 +17,9 @@ def count_neighbouring_mines(board: list) -> list:
         for j in range (len(board[i])):
             if board[i][j] == 1:
                 board[i][j] = 9
-
+    print(board)
         
     #Iniciamos el conteo de las minas en cada posición
-    count_mines = 0 #Iniciamos el contador para guardar el registro de las minas alrededor de la posición
 
     #Creamos una matriz para calcular las 8 posiciones alrededor de una casilla
     directions = [
@@ -31,13 +30,16 @@ def count_neighbouring_mines(board: list) -> list:
 
     for i in range (len(board)):
         for j in range (len(board[i])):
-
+            
+            if board[i][j] == 9:
+                continue
+            count_mines = 0 #Iniciamos el contador para guardar el registro de las minas alrededor de la posición
             for x, y in directions:
                 new_i = i + x
                 new_j = j + y
 
                 #Validamos los bordes
-                if 0 <= new_i < len(board) and 0 <= new_j < len(board[i]):
+                if 0 <= new_i < len(board) and 0 <= new_j < len(board[new_i]):
                     if board[new_i][new_j] == 9:
                         count_mines += 1
             board[i][j] = count_mines
@@ -45,7 +47,9 @@ def count_neighbouring_mines(board: list) -> list:
     
 
 board = [
-    [0, 1, 0],
-    [1, 0, 1]
+[0, 1, 0, 0],
+[0, 0, 1, 0],
+[0, 1, 0, 1],
+[1, 1, 0, 0]
 ]
 print(count_neighbouring_mines(board))
