4bc232f52179a9e44238d8097a10b8b3369dac48
zooth98 <alejozambrano020@gmail.com>
2026-05-17 10:06:14 -0500

    Se crea comando para la creación del esquema de base de datos

--- db_files/schema.sql
+++ db_files/schema.sql
@@ -2,9 +2,22 @@
 
 
 -- TABLA CUSTOMERS
-
+CREATE TABLE customers (
+    id SMALLINT PRIMARY KEY,
+    first_name VARCHAR(64),
+    last_name VARCHAR(64)
+);
 
 -- TABLA CAMPAIGNS
+CREATE TABLE campaigns(
+    id SMALLINT PRIMARY KEY,
+    customer_id SMALLINT,
+    name VARCHAR(64)
+);
 
-
--- TABLA EVENTOS
+-- TABLA EVENTS
+CREATE TABLE events(
+    dt VARCHAR(19),
+    campaing_id SMALLINT,
+    status VARCHAR(64)
+);
